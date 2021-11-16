4a6ef449ecfd6c961f2fe8bbe72a6a800c1e33b7
Udara Liyanage <udaraliyanage@gmail.com>
2021-11-16 20:58:44 +0800

    added screensheeting popup

--- src/pages/OurSolution.js
+++ src/pages/OurSolution.js
@@ -7,16 +7,16 @@ import { LightBox } from "../components/LightBox";
 import axios from "axios";
 
 export function OurSolution() {
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user",
-  };
+  // const videoConstraints = {
+  //   width: 1280,
+  //   height: 720,
+  //   facingMode: "user",
+  // };
 
   const webcamRef = React.useRef(null);
   const [showLightBox, setShowLightbox] = React.useState(false);
   const [capturedImage, setCapturedImage] = React.useState("");
-  const [analysedResponse, setAnalysedResponse] = React.useState(undefined)
+  const [analysedResponse, setAnalysedResponse] = React.useState(undefined);
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setCapturedImage(imageSrc);
@@ -29,11 +29,14 @@ export function OurSolution() {
     setCapturedImage("");
   };
   const submitCapturedImage = async () => {
-    console.log("###", capturedImage)
-    const response = await axios.post('http://localhost:8080/api/ergonomics/index', capturedImage);
-    setAnalysedResponse(response.data)
+    console.log("###", capturedImage);
+    const response = await axios.post(
+      "http://localhost:8080/api/ergonomics/index",
+      capturedImage
+    );
+    setAnalysedResponse(response.data);
     console.log("adheesh", analysedResponse);
-  }
+  };
 
   return (
     <div>
@@ -51,18 +54,132 @@ export function OurSolution() {
         </div>
       </div>
       {showLightBox && (
-        <LightBox width="1280px" onClickClose={setshowLightboxWebcam}>
-          {(!capturedImage && analysedResponse == undefined) && (
-            <Webcam
-              audio={false}
-              height={720}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              width={1100}
-              videoConstraints={videoConstraints}
-            />
-          )}
-          {capturedImage && analysedResponse == undefined && (
+        <LightBox width="82%" onClickClose={setshowLightboxWebcam}>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="camSettingWrapper">
+                <form>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="settings1"
+                      name="settings1"
+                      value="Bike"
+                    />
+                    <label for="settings1">Screen Setting</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="settings2"
+                      name="settings2"
+                      value="Bike"
+                    />
+                    <label for="settings2">Camera Turned On</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="settings3"
+                      name="settings3"
+                      value="Bike"
+                    />
+                    <label for="settings3">Human Position Detected</label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="settings4"
+                      name="settings4"
+                      value="Bike"
+                    />
+                    <label for="settings4">Lightning Checking</label>
+                  </div>
+                  <div className="resetTuneDiv">
+                    <Button btnText={"Reset Tuning"} />
+                  </div>
+                </form>
+
+                <div className="screenSettingsWrapper">
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">Screen Diagnol</div>
+                      <div className="screenSettingValue">13 inc</div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">
+                        Display Resolution
+                      </div>
+                      <div className="screenSettingValue">
+                        1920 * 1080(Full HD)
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">Screen Width</div>
+                      <div className="screenSettingValue">11.3" (28.8 cm)</div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">Screen Height</div>
+                      <div className="screenSettingValue">6.4" (16.2 cm)</div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">Aspect Ratio</div>
+                      <div className="screenSettingValue">1.78:1 (16:9)</div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="screenSettingsDiv">
+                      <div className="screenSettingLabel">Brightness</div>
+                      <div className="screenSettingValue">
+                        <input
+                          type="range"
+                          class="form-range"
+                          id="customRange1"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              {!capturedImage && analysedResponse == undefined && (
+                <Webcam
+                  audio={false}
+                  ref={webcamRef}
+                  screenshotFormat="image/jpeg"
+                  screenshotQuality={1}
+                  // videoConstraints={videoConstraints}
+                />
+              )}
+              {analysedResponse == undefined && (
+                <div className="captureBtnDiv">
+                  <Button
+                    btnText={capturedImage ? "SUBMIT" : "Analyze the Posture"}
+                    solid={true}
+                    onClick={capturedImage ? submitCapturedImage : capture}
+                  />
+
+                  <Button btnText={"Stop Webcam"} />
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* {capturedImage && analysedResponse == undefined && 
             <div>
               <div className="capturedImageDiv">
                 <img src={capturedImage} alt="asd" className="capturedImage" />{" "}
@@ -75,17 +192,13 @@ export function OurSolution() {
                   onClick={recapture}
                 />
               </div>
-            </div>
-          )}
-          {analysedResponse == undefined && <div className="captureBtnDiv">
-            <Button btnText={capturedImage ? "SUBMIT" : "CAPTURE"} solid={true} onClick={capturedImage ? submitCapturedImage : capture} />
-          </div>}
-          {analysedResponse && <div className="resultDiv">
+        </div> } */}
+
+          {/* {analysedResponse && <div className="resultDiv">
             {analysedResponse.score + analysedResponse.message}
-          </div>}
+          </div>}  */}
         </LightBox>
       )}
-
     </div>
   );
 }
